b60a5e05ea3a0da348e1ab9178b0ad688f84dfec
Diego Almeida <dyhalmeida@gmail.com>
2022-09-14 20:32:41 -0300

    feat: add Heading component

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -3,6 +3,7 @@ import { View, Image } from 'react-native';
 import { styles } from './styles';
 
 import logoImage from '../assets/logo-nlw-esports.png'
+import { Heading } from '../components/Heading';
 
 export function Home() {
   return (
@@ -10,6 +11,10 @@ export function Home() {
       <Image
         source={logoImage}
         style={styles.logo}
+      />
+      <Heading 
+        title='Encontre o seu duo!'
+        subtitle='Selecione o game que deseja jogar...'
       />
     </View>
   );
