634602e268969a9c134b761683bf23a2874565ac
Diego Almeida <dyhalmeida@gmail.com>
2022-09-14 21:00:59 -0300

    feat: add GameCard component

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,9 +1,11 @@
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import { styles } from './styles';
-
 import logoImage from '../assets/logo-nlw-esports.png'
 import { Heading } from '../components/Heading';
+import { GameCard } from '../components/GameCard';
+
+import { GAMES } from '../utils/games';
 
 export function Home() {
   return (
@@ -15,6 +17,19 @@ export function Home() {
       <Heading 
         title='Encontre o seu duo!'
         subtitle='Selecione o game que deseja jogar...'
+      />
+
+      <FlatList 
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard 
+            data={item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
       />
     </View>
   );
