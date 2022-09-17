7899f2f74fa1ec9eda714675df59380a231b8466
Diego Almeida <dyhalmeida@gmail.com>
2022-09-17 09:15:53 -0300

    feat: add screen game

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,12 +1,14 @@
+export interface GameParams {
+  id: number
+  title: string
+  bannerUrl: string
+}
+
 export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined
-      game: {
-        id: number
-        title: string
-        bannerUrl: string
-      }
+      game: GameParams
     }
   }
 }
--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,13 +1,52 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { TouchableOpacity, View, Image } from "react-native";
+import { useRoute, useNavigation } from '@react-navigation/native'
+import { Entypo } from '@expo/vector-icons'
+
 import { Background } from "../../components/Background";
 
+import { GameParams } from "../../@types/navigation";
+import { THEME } from "../../theme";
 import { styles } from "./styles";
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from "../../components/Heading";
 
 export function Game() {
+
+  const route = useRoute()
+  const navigation = useNavigation()
+  const game = route.params as GameParams
+
   return (
     <Background>
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Entypo  
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+          <Image 
+            source={logoImg}
+            style={styles.logo}
+          />
+          <View style={styles.right} />
+        </View>
+
+        <Image 
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading 
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar!"
+        />
+      </SafeAreaView>
     </Background>
   );
 }
